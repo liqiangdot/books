79d0090570bea18fd49dd01bcf1f8299b1ac1c20
LiQiang <liqiang.net@gmail.com>
2021-04-06 21:27:06 +0800

    提交，处理：requests.exceptions.ConnectionError

--- main.py
+++ main.py
@@ -105,6 +105,8 @@ class GetOtherFile:
             self.status = -3 # 下载目标文件出错
 
         self.size = os.path.getsize(self.file)
+        global GLOBAL_DOWN_ERR200
+        GLOBAL_DOWN_ERR200.append(self)
         print("第三方站点下载完成(%d)" % (self.size))
 
 class FileSave:
@@ -115,6 +117,31 @@ class FileSave:
         self.url = url  # URL绝对路径
         self.status = 0 # 状态 0(未下载) 1(成功下载)
         self.size = 0	# 文件大小
+
+    def __get_header(self):
+        USER_AGENTS = [
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+            "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+            "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+            "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+            "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+            "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+            "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        ]
+
+        random_agent = USER_AGENTS[random.randint(0, len(USER_AGENTS) - 1)]
+        self.headers = {
+            'User-Agent': random_agent,
+        }
 
     # 记录错误文件
     def __write_err_file(self):
@@ -171,7 +198,7 @@ class FileSave:
     def down_file(self):
         # 判断路径是否存在
         self.__ready_dir()
-
+        self.__get_header()
         # 判断文件是否存在
         global GLOBAL_DOWN_SIZE
         global GLOBAL_DOWN_SUCCE
@@ -182,7 +209,7 @@ class FileSave:
 
         start = time.time()  # 下载开始时间
         try:
-            self.response = requests.get(self.url, stream=True)
+            self.response = requests.get(self.url, headers=self.headers, stream=True)
             size = 0  # 初始化已下载大小
             chunk_size = 1024  # 每次下载的数据大小
             self.response.raise_for_status()
@@ -246,7 +273,10 @@ class FileSave:
 
             str_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
             print("下载总数据量：%.2f(MBit) 当前时间：%s" % (GLOBAL_DOWN_SIZE/(1024 * 1024), str_time))
-        except:
+        except requests.exceptions.ConnectionError as error:
+            self.status = -2 # 连接出错，需要重新连接
+            print("\033[1;31m文件下载失败：" + self.url + "\033[0m")
+        else:
             self.status = -1 # 发生异常
             print("\033[1;31m文件下载失败：" + self.url + "，错误码：" + str(self.response.status_code) + "\033[0m")
             GLOBAL_DOWN_ERROR.append(self)
